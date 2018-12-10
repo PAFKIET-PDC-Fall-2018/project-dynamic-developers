13fd8d903dfc26c2db01eb3d0fff115fd1ac7333
smfasihaly <smfasihali@gmail.com>
2018-12-11 04:21:12 +0500

    Update userfetching.py

--- Source Codes/userfetching.py
+++ Source Codes/userfetching.py
@@ -1,8 +1,8 @@
 import csv
 
 ls = []
-with open('data.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
+with open('data.csv') as csv_file: 
+    csv_reader = csv.reader(csv_file, delimiter=',') 
     line_count = 0
     for row in csv_reader:
         print(row[1])
