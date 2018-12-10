a62d79e286e0eeff8f75ed67978172c90199e6d2
smfasihaly <smfasihali@gmail.com>
2018-12-11 04:06:40 +0500

    Update classifier.py

--- Source Codes/classifier.py
+++ Source Codes/classifier.py
@@ -1,4 +1,4 @@
-from nltk.classify import NaiveBayesClassifier
+from nltk.classify import NaiveBayesClassifier # for sentimental analysis [By S M Fasih Ali 60838] 
 
 
 def word_feats(words):
@@ -7,19 +7,20 @@ def word_feats(words):
 
 arr = []
 def classify(sentence):
+    # providing vocab for positivity negativity and neutral words [By S M Fasih Ali 60838] 
     positive_vocab = ['awesome', 'outstanding', 'fantastic', 'terrific', 'good', 'nice', 'great', ':)']
     negative_vocab = ['bad', 'terrible', 'useless', 'not', 'hate', ':(']
     neutral_vocab = ['movie', 'the', 'sound', 'was', 'is', 'actors', 'did', 'know', 'words', 'not']
-
+    
     positive_features = [(word_feats( pos ), 'pos') for pos in positive_vocab]
     negative_features = [(word_feats( neg ), 'neg') for neg in negative_vocab]
     neutral_features = [(word_feats( neu ), 'neu') for neu in neutral_vocab]
-
+# training set [By S M Fasih Ali 60838] 
     train_set = negative_features + positive_features + neutral_features
-
+#classifiyng train set [By S M Fasih Ali 60838] 
     classifier = NaiveBayesClassifier.train( train_set )
 
-    # Predict
+    # Predicting sentiments [By S M Fasih Ali 60838] 
     neg = 0
     pos = 0
     sentence = sentence.lower()
