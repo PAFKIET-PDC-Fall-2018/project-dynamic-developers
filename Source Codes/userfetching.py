0698bde548788c7086f6a35f70a0d3374afe984a
M Ghous Sarwar <ghoussarwar@gmail.com>
2018-12-11 04:53:54 +0500

    [By Muahammad Ghous 59455]

--- Source Codes/userfetching.py
+++ Source Codes/userfetching.py
@@ -1,6 +1,7 @@
 import csv
 
 ls = []
+#we have a very large file of 480 MB so we extract username name from it  [By Muahammad Ghous 59455]
 with open('data.csv') as csv_file: 
     csv_reader = csv.reader(csv_file, delimiter=',') 
     line_count = 0
@@ -8,6 +9,7 @@ with open('data.csv') as csv_file:
         print(row[1])
         ls.append(row[1]+"\n")
 leng=len(ls)
+#nov saving username to username .csv [By Muahammad Ghous 59455]
 for i in range(1):
     with open('resources/username.csv', 'a') as fd:
         fd.writelines(ls)
