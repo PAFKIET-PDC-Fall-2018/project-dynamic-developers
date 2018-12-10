3e7785368a09c4ed6a6366ddb9bd57565ae579e0
smfasihaly <smfasihali@gmail.com>
2018-12-11 04:15:52 +0500

    Update sss.py

--- Source Codes/sss.py
+++ Source Codes/sss.py
@@ -1,18 +1,18 @@
 
 
+# importing libraries [By S M Fasih Ali 60838] 
+import nltk 
+from nltk.corpus import sentiwordnet as swn # for sentimental analysis [By S M Fasih Ali 60838] 
 
-import nltk
-from nltk.corpus import sentiwordnet as swn
 
-
-def analysis(doc):
-  sentences = nltk.sent_tokenize(doc)
-  stokens = [nltk.word_tokenize(sent) for sent in sentences]
+def analysis(doc): # takes one aurgumentdoc which is the text of tweets in our case  [By S M Fasih Ali 60838] 
+  sentences = nltk.sent_tokenize(doc)  # tokenize the tweets into sentences [By S M Fasih Ali 60838] 
+  stokens = [nltk.word_tokenize(sent) for sent in sentences] # tokenize sentence into words [By S M Fasih Ali 60838] 
   taggedlist = []
   for stoken in stokens:
     taggedlist.append(nltk.pos_tag(stoken))
-    wnl = nltk.WordNetLemmatizer()
-    score_list = []
+    wnl = nltk.WordNetLemmatizer() # convert all the verbs to their base form [By S M Fasih Ali 60838] 
+    score_list = [] # lsit of score of sentence [sentiments scores] [By S M Fasih Ali 60838] 
     for idx, taggedsent in enumerate(taggedlist):
       score_list.append([])
       for idx2, t in enumerate(taggedsent):
@@ -29,8 +29,8 @@ def analysis(doc):
         else:
           newtag = ''
         if (newtag != ''):
-          synsets = list(swn.senti_synsets(lemmatized, newtag))
-          # Getting average of all possible sentiments, as you requested
+          synsets = list(swn.senti_synsets(lemmatized, newtag)) # find synonyms of words for simplicity to analize [By S M Fasih Ali 60838]  
+          # Getting average of all possible sentiments, as  requested [By S M Fasih Ali 60838] 
           score = 0
           if (len(synsets) > 0):
             for syn in synsets:
@@ -40,15 +40,14 @@ def analysis(doc):
     # print(score_list)
     sentence_sentiment = []
 
-    for score_sent in score_list:
+    for score_sent in score_list: # sentiment analysis of each word [By S M Fasih Ali 60838]  
       if len(score_sent) == 0:
         sentence_sentiment.append(0)
         continue
-      sentence_sentiment.append(sum([word_score for word_score in score_sent]) / len(score_sent))
-
-    if sentence_sentiment[0] > 0.03:
+      sentence_sentiment.append(sum([word_score for word_score in score_sent]) / len(score_sent)) # dividing score of sum of all words by score of sentece [By S M Fasih Ali 60838] 
+    if sentence_sentiment[0] > 0.03: # if greater than 0.03 positive [By S M Fasih Ali 60838] 
       return ("Happy")
-    elif sentence_sentiment[0] <-0.03:
+    elif sentence_sentiment[0] <-0.03: # if lest than -0.05 negative [By S M Fasih Ali 60838] 
       return ("sad")
-    else:
-      return ("Normal")
+    else:# 
+      return ("Normal") #else neutral [By S M Fasih Ali 60838] 
