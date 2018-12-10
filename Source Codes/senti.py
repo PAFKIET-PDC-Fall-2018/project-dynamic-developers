0950db7454e6b767aea12eb40c8a9dcb8bdd09fa
smfasihaly <smfasihali@gmail.com>
2018-12-11 03:10:02 +0500

    Add files via upload

--- Source Codes/senti.py
+++ Source Codes/senti.py
@@ -1,19 +1,7 @@
 from twitter_scraper import get_tweets
-
-import classifier
-
-usr= 'realDonaldTrump'
-
-for tweet in get_tweets(usr, pages=1):
-    print("tweet by: @",usr)
-    print("Post:")
-    post = tweet['text']
-    print(post)
-    pn = classifier.classify(post)
-
-    print(pn[0])
-    print(pn[1])
-
-    input()
-
-
+def scrape(usr,pg):
+  try:
+    arr = get_tweets(usr,pages=pg)
+    return [arr,usr]
+  except:
+    print("user is blocked")
